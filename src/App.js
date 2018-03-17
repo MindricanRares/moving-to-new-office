dc307c080cfd17a8c4c414b2d631569dca3959d2
Rares <mindricanmihai@gmail.com>
2018-03-17 10:09:24 +0200

    - fixed countdown issue

--- src/App.js
+++ src/App.js
@@ -5,39 +5,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     window.$(this.refs.list).fadeOut();
-    function countdown() {
-      var now = new Date();
-      var eventDate = new Date(2018, 9, 1);
-
-      var currentTime = now.getTime();
-      var eventTime = eventDate.getTime();
-
-      var remTime = eventTime - currentTime;
-
-      var seconds = Math.floor(remTime / 1000);
-      var minutes = Math.floor(seconds / 60);
-      var hours = Math.floor(minutes / 60);
-      var days = Math.floor(hours / 24);
-
-      hours %= 24;
-      minutes %= 60;
-      seconds %= 60;
-
-      hours = hours < 10 ? "0" + hours : hours;
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-
-      document.getElementById("days").textContent = days;
-      document.getElementById("hours").textContent = hours;
-      document.getElementById("minutes").textContent = minutes;
-      document.getElementById("seconds").textContent = seconds;
-    }
-
-
-  
   }
+  countdown=()=> {
+    var now = new Date();
+    var eventDate = new Date(2018, 9, 1);
 
+    var currentTime = now.getTime();
+    var eventTime = eventDate.getTime();
+
+    var remTime = eventTime - currentTime;
+
+    var seconds = Math.floor(remTime / 1000);
+    var minutes = Math.floor(seconds / 60);
+    var hours = Math.floor(minutes / 60);
+    var days = Math.floor(hours / 24);
+
+    hours %= 24;
+    minutes %= 60;
+    seconds %= 60;
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+  }
   componentDidMount(){
+    setInterval(() => {
+      this.countdown()
+    }, 1000);
     const url = 'http://adevarul.ro/ex/yrssv2'
     const url2 ='http://www.brasovultau.ro/feed/';
     const textarea = document.getElementById('rss-part')
